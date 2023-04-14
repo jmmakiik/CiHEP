bd5ef8f37d076a7a54c41e37f5e278e3bdb6425a
jmmakiik <jere.maki-ikola@helsinki.fi>
2023-04-14 06:05:19 +0300

    Completed and tested exercise 10

---
+++ Ex10/jmmakiik/DetectorModifier.sh
@@ -0,0 +1,2 @@
+#sed -i 's/WATER/Pb/' $G4INSTALL/share/Geant4/examples/basic/B4/B4a/src/DetectorConstruction.cc
+sed -i 's/Pb/WATER/' $G4INSTALL/share/Geant4/examples/basic/B4/B4a/src/DetectorConstruction.cc
